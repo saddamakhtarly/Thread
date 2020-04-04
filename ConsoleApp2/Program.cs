88b30438671d5f7541006f0da9a882f8791c89b2
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2020-04-04 14:16:02 +0500

    update

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,8 +13,10 @@ namespace ConsoleApp2
         {
             bool stopped = false;            
             Thread t = new Thread(new ThreadStart(()=> {
+                
                 while (!stopped)
                 {
+                    //Print after 500milisecond again and again
                     Console.WriteLine("Running...");
                     Thread.Sleep(500);
                 }
