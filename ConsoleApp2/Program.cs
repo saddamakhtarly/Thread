10d4f14a97a153613ff79cff90a3e72e6a1e1ed9
Saddam Akhtar <saddamakhtar.ly@gmail.com>
2020-04-04 14:08:49 +0500

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stopped = false;            
+            Thread t = new Thread(new ThreadStart(()=> {
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(500);
+                }
+            }));
+            t.Start();            
+            Console.ReadKey();
+            stopped = true;            
+            
+            Console.WriteLine("done");
+            Console.ReadKey();
+        }
+    }
+}
